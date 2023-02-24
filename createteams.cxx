5390aba7773af7b3d4398364b978f4bad4c0bfef
user <github.ecj8p@8alias.com>
2023-02-23 23:06:15 -0800

    json output

--- createteams.cxx
+++ createteams.cxx
@@ -9,21 +9,33 @@ int main(int argc, char** argv) {
     std::cout<<yawr.numPlayers();
     std::cout<<std::endl;
     std::cout<<yawr.winPercentages(1,1);
-    int i,j;
+    
+    int i;
+    int j;
     int np=yawr.numPlayers();
+
     PriorityQueue ganr(np);
-    for(i=0;i<np;i++){
-        for(j=i+1;j<np;j++){
-            ganr.insert(std::abs(yawr.winPercentages(i,j)-50.0));
+
+    for(i = 0; i < np; i++){
+        for(j = i + 1; j < np; j++){
+            ganr.insert(std::abs(yawr.winPercentages(i,j) - 50.0));
         }
     }
+
     KeyValuePair xs;
     nlohmann::json opn;
-    for(i=0;i<np;i++){
-        xs = ganr.removeMin();
-        opn
 
+    for(int i = 0; i < np; i++) {
+        xs = ganr.removeMin();
+        opn = nlohmann::json::object();
+
+        opn["teams"]["playerOne"] = xs.second.first;
+        opn["teams"]["playerTwo"] = xs.second.second;
+        opn["teams"]["winPercentage"] = yawr.winPercentages(xs.second.first, xs.second.second);
+
+        std::cout << opn.dump(2) << std::endl;
     }
-    //std::cout<<ganr.JSON().dump(2)<<std::endl;
-    
+
+    return 0;
 }
+
